dd216ac2ce0c0ce3278367044dd50d762692cc1c
Mohammed Asif <diraar@gmail.com>
2020-05-13 01:59:44 +0100

    put hardcoded values as constants

---
+++ Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChipEight {
+  class Consts {
+    public const string Title = "Chip 8 Emulator";
+    public const int Memory = 4096;
+    public const int Width = 64;
+    public const int Height = 32;
+    public const int Scale = 20;
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
-using System.Diagnostics;
 
 namespace ChipEight {
   class Program {
     static void Main(string[] args) {
-      using var window = new RenderWindow(new VideoMode(640, 320), "Chip 8 Emulator");
+      var videoMode = new VideoMode(Consts.Width * Consts.Scale, Consts.Height * Consts.Scale);
+      using var window = new RenderWindow(videoMode, Consts.Title);
       window.Closed += (s, e) => window.Close();
 
       var shape = new RectangleShape(new Vector2f(100, 100)) {
@@ -14,25 +14,11 @@ namespace ChipEight {
         Position = new Vector2f(100, 100)
       };
 
-      var fpsText = new Text("FPS", new Font("consola.ttf")) {
-        CharacterSize = 22,
-        FillColor = Color.Red,
-        Style = Text.Styles.Regular,
-        Position = new Vector2f(10, 2)
-      };
-
-      var frameCount = 0L;
-      var clock = new Stopwatch();
-      clock.Start();
-
       while (window.IsOpen) {
         window.DispatchEvents();
         window.Clear();
         window.Draw(shape);
-        window.Draw(fpsText);
         window.Display();
-
-        fpsText.DisplayedString = $"FPS:{(int) (frameCount++ / clock.Elapsed.TotalSeconds)}";
       }
     }
   }
