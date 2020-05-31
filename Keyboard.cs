476d2b6696483dda012519be9429586270a4917c
Mohammed Asif <diraar@gmail.com>
2020-05-31 16:15:24 +0100

    use locks on screen and keyboard arrays as a precaution

--- Keyboard.cs
+++ Keyboard.cs
@@ -3,21 +3,29 @@
 namespace ChipEight {
   class Keyboard {
     private readonly bool[] _keyStates = new bool[Consts.KeyCount];
-    private TaskCompletionSource<byte> _tcs;
+    private static readonly object _lockObj = new object();
+    private static TaskCompletionSource<byte> _tcs;
 
     public void Press(byte key) {
-      _keyStates[key] = true;
-      if (_tcs != null) {
-        _tcs.SetResult(key);
+      lock (_lockObj) {
+        _keyStates[key] = true;
       }
     }
 
     public void Release(byte key) {
-      _keyStates[key] = false;
+      lock (_lockObj) {
+        _keyStates[key] = false;
+      }
+
+      if (_tcs != null && !_tcs.Task.IsCompleted) {
+        _tcs.SetResult(key);
+      }
     }
 
     public bool IsKeyPressed(byte key) {
-      return _keyStates[key];
+      lock (_lockObj) {
+        return _keyStates[key];
+      }
     }
 
     public Task<byte> WaitForKeyPressAsync() {
--- Screen.cs
+++ Screen.cs
@@ -3,17 +3,24 @@
 namespace ChipEight {
   class Screen {
     private readonly bool[,] _pixels = new bool[Consts.Width, Consts.Height];
+    private static readonly object _lockObj = new object();
 
     public bool Set(int x, int y) {
-      return !(_pixels[x, y] ^= true);
+      lock (_lockObj) {
+        return !(_pixels[x, y] ^= true);
+      }
     }
 
     public bool IsSet(int x, int y) {
-      return _pixels[x, y];
+      lock (_lockObj) {
+        return _pixels[x, y];
+      }
     }
 
     public void Clear() {
-      Array.Clear(_pixels, 0, _pixels.Length);
+      lock (_lockObj) {
+        Array.Clear(_pixels, 0, _pixels.Length);
+      }
     }
   }
 }
